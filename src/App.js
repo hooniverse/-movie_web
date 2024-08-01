3f6a1466705d35090deac62a76e95f78d3e8447a
hooniverse <sung010326@gmail.com>
2024-08-01 22:12:01 +0900

    Route Path 수정

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <Routes>
       <Route path={`/movie/:id`} element={<Detail/>}/>
     
-      <Route path="/" element={<Home/>}/>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}/>
     </Routes>
   </Router>;
 }
